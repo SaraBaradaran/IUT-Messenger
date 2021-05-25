8258bfe0d385238b94b6abb04b1b378f0dd39f60
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 20:08:53 +0430

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,14 @@
+/****************************************************************   
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Jun 2018 				
+# 	Module Name:    main.cpp	
+# 	Project Name:   IUT_Messenger	
+#								
+#								
+****************************************************************/
+
 #include <QApplication>
 
 #include "chatroom.h"
