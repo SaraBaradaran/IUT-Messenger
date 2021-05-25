d078d52e4c7a0258e5d94397222e114e45feabb0
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2021-05-25 19:01:02 +0430

    Update logout.h

--- headers/logout.h
+++ headers/logout.h
@@ -1,3 +1,14 @@
+#################################################################   
+#								
+# 								
+# 	Creator Name:   Sara Baradaran, Mahdi Heidari		
+# 	Create Date:    Jun 2018 				
+# 	Module Name:    logout.h
+# 	Project Name:   IUT_Messenger	
+#								
+#								
+#################################################################
+
 #ifndef LOGOUT_H
 #define LOGOUT_H
 
